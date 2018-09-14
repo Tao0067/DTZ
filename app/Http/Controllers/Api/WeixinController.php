cc1c46fe52cbb78d2f877fb34dcbfd7a0d4e17dc
CT <1404451270@qq.com>
2018-09-14 17:21:03 +0800

    accessToken

--- app/Http/Controllers/Api/WeixinController.php
+++ app/Http/Controllers/Api/WeixinController.php
@@ -62,7 +62,7 @@ class WeixinController extends ApiController
     {
         $accessToken = $this->accessToken();
 
-        return $this->success($accessToken);
+        return $this->success(['accessToken' => $accessToken]);
     }
 
 
