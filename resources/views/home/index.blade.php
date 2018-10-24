71987ef984b0697a04c0a40cbf568f96d51028ac
CT <1404451270@qq.com>
2018-10-24 14:33:45 +0800

    home

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.body')
+@section('content')
+    <h1>欢迎来到TIC博客</h1>
+@stop
+
---
+++ resources/views/layouts/body.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>@yield('title','TIC的博客') - TIC的博客</title>
+</head>
+<body class="container">
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation" style="padding-left:100px">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">W3Cschool</a>
+        </div>
+
+        <ul class="nav justify-content-end">
+            DSDS
+        </ul>
+
+
+    </nav>
+
+
+
+</body>
+    <script  type="text/javascript" src="/js/app.js" ></script>
+</html>
